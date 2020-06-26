69ea3810b3513637eb5487bf1bcf69600e75777d
andres ortega <andres.ortega08@inacalmail.cl>
2020-06-26 16:28:25 -0400

    ultimo commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,9 +30,7 @@ class HomeController extends Controller
     {
         $i = 0;
         $proyectos = Proyecto::orderby('id', 'desc')->paginate(5);
-
-        //$phone = User::find(1)->proyecto;
-
+        //$phone = User::find(1)->proyecto; https://laravel.com/docs/7.x/pagination#paginating-eloquent-results
         return view('home', [
             'proyectos' => $proyectos,
             '$i' => $i
@@ -53,76 +51,74 @@ class HomeController extends Controller
 
     public function donar2(Request $request)
     {
-        
-    
-        
-        
+
+
+
+
         $user = Auth::user();
         $user_id = $user->id;
-    //    var_dump($user_id);
-     //   echo '<br>';
+        //    var_dump($user_id);
+        //   echo '<br>';
         $proyecto_id  =  $request->input('id');
-      //  var_dump($proyecto_id);
-     //   echo '<br>';
+        //  var_dump($proyecto_id);
+        //   echo '<br>';
         $name  =  $request->input('name');
-      //  var_dump($name);
-     //   echo '<br>';
+        //  var_dump($name);
+        //   echo '<br>';
         $usuario  =  $request->input('usuario');
-       // var_dump($usuario);
-      //  echo '<br>';
+        // var_dump($usuario);
+        //  echo '<br>';
         $MetodoPago  =  $request->input('MetodoPago');
-       // var_dump($MetodoPago);
-       // echo '<br>';
+        // var_dump($MetodoPago);
+        // echo '<br>';
         $monto  =  $request->input('capital');
-       // var_dump($monto);
-      
+        // var_dump($monto);
 
-         $validatedData = $request->validate([
-           'id' => 'required',
-          'MetodoPago' => 'required',
-          'capital' => 'required|numeric|integer|min:4',
-          
-         ]);
-         $mensaje='Se a realizado con éxito la donación';
-         $proyecto2= Proyecto::where('id', $proyecto_id)->get()->first();
-         if($monto>999){        
-        // var_dump($monto);   
-        $donacion = new Donacion();
-        $donacion->user_id=$user_id;
-        $donacion->proyecto_id=$proyecto_id;
-        $donacion->monto=  $monto ;
-        $donacion->descripcion_pago=$MetodoPago;
-        $donacion->save();
-        return  view('proyecto.donar',[
-            'proyecto' => $proyecto2,
-            'message'=>$mensaje
+
+        $validatedData = $request->validate([
+            'id' => 'required',
+            'MetodoPago' => 'required',
+            'capital' => 'required|numeric|integer|min:4',
+
         ]);
-
-        }else{
-            $mensaje='Ingrese un valor valido';
-            redirect()->route('donar',['proyecto'=>$proyecto2])->with([
+        $mensaje = 'Se a realizado con éxito la donación';
+        $proyecto2 = Proyecto::where('id', $proyecto_id)->get()->first();
+        if ($monto > 999) {
+            // var_dump($monto);   
+            $donacion = new Donacion();
+            $donacion->user_id = $user_id;
+            $donacion->proyecto_id = $proyecto_id;
+            $donacion->monto =  $monto;
+            $donacion->descripcion_pago = $MetodoPago;
+            $donacion->save();
+            return  view('proyecto.donar', [
+                'proyecto' => $proyecto2,
+                'message' => $mensaje
+            ]);
+        } else {
+            $mensaje = 'Ingrese un valor valido';
+            redirect()->route('donar', ['proyecto' => $proyecto2])->with([
                 'message' => $mensaje
             ]);
         }
 
-  
-         $mensaje='Error';
-            redirect()->route('donarx',['proyecto'=>$proyecto2])->with([
-                'message' => $mensaje
-            ]);
-      
-            
-            return view('proyecto.donar',['proyecto'=>$proyecto2])->with([
-                'messageAlerta' => "Error en el monto  ingresado"
-            ]);
-    }     
-    
-    public function Employe(){
 
-        $trabajador= Trabajador::orderby('id', 'asc')->paginate(5);;
-    
-        return view('user.workers',['trabajadores'=>$trabajador]);
+        $mensaje = 'Error';
+        redirect()->route('donarx', ['proyecto' => $proyecto2])->with([
+            'message' => $mensaje
+        ]);
 
+
+        return view('proyecto.donar', ['proyecto' => $proyecto2])->with([
+            'messageAlerta' => "Error en el monto  ingresado"
+        ]);
     }
-    
+
+    public function Employe()
+    {
+
+        $trabajador = Trabajador::orderby('id', 'asc')->paginate(5);;
+
+        return view('user.workers', ['trabajadores' => $trabajador]);
+    }
 }
